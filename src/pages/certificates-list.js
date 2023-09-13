900883d3ca2fadf0b81f9c12e405bd87ecbccdd3
Olha Krasnokutska <olechka310588@gmail.com>
2023-09-13 16:45:58 +0200

    add certificates

--- src/pages/certificates-list.js
+++ src/pages/certificates-list.js
@@ -1,8 +1,63 @@
 const certificates = [
 
+        {
+            image: "https://udemy-certificate.s3.amazonaws.com/image/UC-a3d81799-74f3-40bc-9410-3cc985144704.jpg?v=1694286484000",
+            alt: "Udemy Certificate",
+            link: "https://ibmcsr.udemy.com/certificate/UC-a3d81799-74f3-40bc-9410-3cc985144704/",
+            },
+            {
+                image: "https://i.ibb.co/qM5XG0n/Interpersonal-skills.png",
+                alt: "IBM SkillsBuild Certificate",
+                link: "https://skills.yourlearning.ibm.com/certificate/ILB-GYEQRZJQNMVJ32MB",
+                },
+                {
+                    image: "https://i.ibb.co/F6kZZQH/Presentation.png",
+                    alt: "IBM SkillsBuild Certificate",
+                    link: "https://skills.yourlearning.ibm.com/certificate/ILB-QPDKNWEKJEPN2QPX",
+                    },
+        {
+            image: "https://i.ibb.co/NjVh8zb/WEB-foundation.png",
+            alt: "IBM SkillsBuild Certificate",
+            link: "https://skills.yourlearning.ibm.com/certificate/PLAN-8749C02A78EC",
+            },
+            {
+                image: "https://i.ibb.co/4pk82Y7/Solving-problems.png",
+                alt: "IBM SkillsBuild Certificate",
+                link: "https://skills.yourlearning.ibm.com/certificate/ILB-NKQEJMVEMYDQ3GYE",
+                },
+    {
+        image: "https://i.ibb.co/7tfx8Tz/WEB-course.png",
+        alt: "IBM SkillsBuild Certificate",
+        link: "https://skills.yourlearning.ibm.com/certificate/PLAN-34DCBB76FFA3",
+        },
+    {
+        image: "https://i.ibb.co/t8CphhL/Collaborate.png",
+        alt: "IBM SkillsBuild Certificate",
+        link: "https://skills.yourlearning.ibm.com/certificate/ILB-NKQEJMVEKJJQ3YJM",
+        },
+    {
+        image: "https://i.ibb.co/z24H7cx/Quality-work.png",
+        alt: "IBM SkillsBuild Certificate",
+        link: "https://skills.yourlearning.ibm.com/certificate/ILB-NKQEJMVENXWRPN8D",
+        },
+    {
+        image: "https://s3.amazonaws.com/shecodesio-production/students/certificates/000/099/304/original/99304.png?1671993143",
+        alt: "SheCodesReact Certificate",
+        link: "https://www.shecodes.io/certificates/16fa30207546d1a141d46b9fab356ef9",
+        },
+    {
+        image: "https://s3.amazonaws.com/shecodesio-production/students/certificates/000/091/995/original/result.png?1662640117",
+        alt: "SheCodesResponsive Certificate",
+        link: "https://www.shecodes.io/certificates/26519defebf300d02b9bb79e80e92700",
+        },
+    {
+        image: "https://s3.amazonaws.com/shecodesio-production/students/certificates/000/079/167/original/79167.png?1665642857",
+        alt: "SheCodesPlus Certificate",
+        link: "https://www.shecodes.io/certificates/490a7f7469402548a237e92694a21ecd",
+        },
     {
     image: "https://s3.amazonaws.com/shecodesio-production/students/certificates/000/075/544/original/result.png?1653415777",
-    alt: "SheCodes",
+    alt: "SheCodesBasics Certificate",
     link: "https://www.shecodes.io/certificates/80299d9242ed7b377ab4437ce47b23f1",
     }
 ];
--- src/pages/certificates.js
+++ src/pages/certificates.js
@@ -5,8 +5,8 @@ import Card from 'react-bootstrap/Card';
 
 function createCard(certificates) {
     return (
-      <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src={certificates.image} alt={certificates.alt} />
+      <Card style={{ width: '25rem' }}>
+        <Card.Img variant="top" src={certificates.image} alt={certificates.alt} style={{height:"250px"}} />
         <Card.Body>
           <Button style={{ backgroundColor:"#d7caae", borderColor:"#d7caae"}}variant="primary"><a href={certificates.link} target="blank">Go to certificate</a></Button>
         </Card.Body>
@@ -17,8 +17,6 @@ function createCard(certificates) {
   function Certificates () {
     return(
         <div className="container">
-
-        <p>page in process...</p>
             {certificates.map(createCard)}
         </div>
     );
